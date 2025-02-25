402c5f0af27ffd2b3105c1736796493fa5d5f1cc
Alexandre H. <holy.cow3728@gmail.com>
2025-02-25 13:22:19 -0300

    Update heranca.js

--- heranca.js
+++ heranca.js
@@ -6,6 +6,24 @@ class Pessoa{
     apresentar = () => console.log(`Olá eu sou ${this.nome} e tenho ${this.idade} anos de idade`)
 }
 
-const alex = new Pessoa("Alexandre" , 17)
+class Aluno extends Pessoa{
+    estudar = () => console.log(`${this.nome} está estudando.`)
+}
+
+class Professor extends Pessoa{
+    darAula = () => console.log(`${this.nome} está dando aula.`)
+}
+
+const alex = new Aluno("Alexandre" , 17)
+const rafa = new Aluno("Rafael" , 17)
+const raul = new Professor("Raul" , 28)
+const nadja = new Professor("Nadja" , 37)
 
 alex.apresentar()
+alex.estudar()
+rafa.apresentar()
+rafa.estudar()
+raul.apresentar()
+raul.darAula()
+nadja.apresentar()
+nadja.darAula()
